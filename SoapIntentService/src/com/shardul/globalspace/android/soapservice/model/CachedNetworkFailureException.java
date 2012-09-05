bf4c2e73a17916b3faaf156788b6b6e1416b041c
Shardul Prabhu <shardulsp@gmail.com>
2012-09-05 21:47:08 +0530

    SOAPIntentService
    Removed unused code.

--- SoapIntentService/src/com/shardul/globalspace/android/soapservice/model/CachedNetworkFailureException.java
+++
@@ -1,8 +0,0 @@
-package com.shardul.globalspace.android.soapservice.model;
-
-import java.io.IOException;
-
-public class CachedNetworkFailureException extends IOException{
-
-	private static final long serialVersionUID = 5984265956398329340L;
-}
